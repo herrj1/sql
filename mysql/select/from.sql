80baa3ecc0962731e5a4e546fec971e5d72d7b17
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-06-26 22:25:57 -0400

    Added generic implementations

---
+++ mysql/select/from.sql
@@ -0,0 +1,3 @@
+--Example
+
+Select * from customers;
---
+++ mysql/select/groupby.sql
@@ -0,0 +1,6 @@
+--Example
+
+select count(c.id), c.name, c.salary, c.department from customers c
+where c.salary > 50000
+group by c.department
+having count(c.id) > 10;
