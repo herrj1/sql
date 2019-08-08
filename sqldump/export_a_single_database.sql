11aad6afa9b327f46c72c18453cc4f909078d75b
Jonathan <38092600+herrj1@users.noreply.github.com>
2019-08-07 23:55:17 -0400

    Update export_a_single_database.sql

--- sqldump/export_a_single_database.sql
+++ sqldump/export_a_single_database.sql
@@ -10,4 +10,4 @@ mysqldump -d -h localhost -u root -p password databasename > customdatabase_name
 
 mysqldump -d -h localhost -u root -p password databasename > customdatabase_name_date12324.sql
 
-mysqldump -d -h localhost -u root -p password databasenames > customdatabase_name_date1324.sql
+mysqldump -d -h localhost -u root -p password databasenames > customdatabase_name_date134.sql
