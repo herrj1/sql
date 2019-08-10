8abebc8dd12b921a3338a7f89b70bcfad0adc1e8
Jonathan <38092600+herrj1@users.noreply.github.com>
2019-08-09 23:41:01 -0400

    Update groupby.sql

--- sqlserver/select/groupby.sql
+++ sqlserver/select/groupby.sql
@@ -4,3 +4,10 @@ select count(c.id), c.name, c.salary, c.department from customers c
 where c.salary > 60000
 group by c.department
 having count(c.id) > 10;
+
+
+
+select count(c.id), c.name, c.salary, c.department from customers c
+where c.salary > 60000
+group by c.restaurant
+having count(c.id) > 10;
