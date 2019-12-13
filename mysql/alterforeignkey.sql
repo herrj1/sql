146e551f2ac58ec6504356384672687bb83b4246
Jonathan <38092600+herrj1@users.noreply.github.com>
2019-12-12 23:45:20 -0500

    Update alterforeignkey.sql

--- mysql/alterforeignkey.sql
+++ mysql/alterforeignkey.sql
@@ -14,3 +14,40 @@ ALTER TABLE `person`
 		ON DELETE CASCADE
 ON UPDATE CASCADE;
 Alter table persons ADD Address2 varchar(244)
+
+
+
+ALTER TABLE `person`
+
+        ADD CONSTRAINT `fk_user_map_details`
+
+        FOREIGN KEY IF NOT EXISTS (`stid`)
+
+                REFERENCES `user`(`id`)
+
+                ON DELETE CASCADE
+
+ON UPDATE CASCADE;
+
+Alter table persons ADD Address2 varchar(244)
+
+
+
+
+
+ALTER TABLE `person`
+
+        ADD CONSTRAINT `fk_user_map_details`
+
+        FOREIGN KEY IF NOT EXISTS (`stid`)
+
+                REFERENCES `user`(`id`)
+
+                ON DELETE CASCADE
+
+ON UPDATE CASCADE;
+
+Alter table persons ADD Address2 varchar(244)
+
+
+
