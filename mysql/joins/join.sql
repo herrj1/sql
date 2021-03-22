6466de62ea1cf2a72c79347acea7c070a49b7fc7
Jonathan <38092600+herrj1@users.noreply.github.com>
2021-03-21 21:52:52 -0400

    Update join.sql

--- mysql/joins/join.sql
+++ mysql/joins/join.sql
@@ -1,7 +1,11 @@
 select s.firstname, s.lastname, s.studentid
+
 from student r
+
 join course c on s.id = c.studentid
+
 join courseavailability i on c.courseid = i.id
+
 where r.id = 1;
 
 select s.firstname, s.lastname, s.employeeid
