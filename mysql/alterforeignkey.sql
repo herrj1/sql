50ef3558fa868d45ab4581793ae4acdb417e4151
Jonathan <38092600+herrj1@users.noreply.github.com>
2021-03-31 20:23:45 -0400

    Update alterforeignkey.sql

--- mysql/alterforeignkey.sql
+++ mysql/alterforeignkey.sql
@@ -2,6 +2,8 @@ ALTER TABLE `doc`
 	ADD CONSTRAINT `fk_user_map_completedapps`
 	FOREIGN KEY IF NOT EXISTS (`stid`)
 		REFERENCES `user`(`id`)
+		
+		
 		ON DELETE CASCADE
 ON UPDATE CASCADE;
 ALTER TABLE `person`
